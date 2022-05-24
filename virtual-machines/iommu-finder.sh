e6681b803951056c5bd9f6c0ec2f2678ef5ad817
max <admin@cloudydev.net>
2022-05-24 08:15:34 +0000

    Docs: update notes

--- virtual-machines/iommu-finder.sh
+++ virtual-machines/iommu-finder.sh
@@ -2,6 +2,6 @@
 shopt -s nullglob
 for d in /sys/kernel/iommu_groups/{0..64}/devices/*; do
     n=${d#*/iommu_groups/*}; n=${n%%/*}
-    printf 'IOMMU Group %s ' "$n"
+    printf 'IOMMU Group %s ' "$n" 
     lspci -nns "${d##*/}"
 done;
