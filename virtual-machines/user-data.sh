bd7d00c99ca4e3b34ca56dcbb339f1409814f80a
cloudymax <admin@cloudydev.net>
2022-08-14 09:38:28 +0000

    fixing ruby and brew errors

--- virtual-machines/user-data.sh
+++ virtual-machines/user-data.sh
@@ -142,7 +142,7 @@ packages:
 runcmd:
   - sudo -u ${VM_USER} echo "export PATH=\"/home/${VM_USER}/.local/bin:\$PATH\"" >> /home/${VM_USER}/.profile 
   - git clone https://github.com/cloudymax/pxeless.git
-  - pxeless/provisioner/provision.sh --ansible-user max
+  - sudo -u max pxeless/provisioner/provision.sh --ansible-user max --profile kind-host
 EOF
 
 log " - Done."
