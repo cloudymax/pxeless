a3be2daf4b8851e7a40f2f51aea49fa83b5cfb3b
cloudymax <admin@cloudydev.net>
2022-08-17 17:13:23 +0000

    pip install causing issues

--- provisioner/provision.sh
+++ provisioner/provision.sh
@@ -115,7 +115,7 @@ deps() {
     log "🔎 Checking for required utilities..."
     if [[ ! -x "$(command -v pip3)" ]]; then
        log "💥 python3-pip is not installed. Installing"
-       sudo apt-get install --yes python3-pip
+       sudo apt-get install python3-pip
     else    
        log "✅ python3-pip installed."
     fi
