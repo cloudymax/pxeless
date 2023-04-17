99d88760c76082265955d8c4d6975aced2642b80
Max! <admin@cloudydev.net>
2023-04-17 09:30:27 +0200

    Added comments, citation, apt -> apt-get
    - Added a few contextual comments that may be useful for newer users 🤓 
    - Changed `apt` to `apt-get` because I'm a coward and the `WARNING: apt does not have a stable CLI interface. Use with caution in scripts.` message still scares me 😨 💻 🔥 
    - Added a citation by name for the author of the referenced article

--- extras/installer-sample.sh
+++ extras/installer-sample.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
+# An example script for installing packages into a chrooted squashfs
+# The article below provides more context on this process.
+# How to Create Custom Debian Based ISO by Alex M. Schapelle
 # Ref: https://dev.to/otomato_io/how-to-create-custom-debian-based-iso-4g37
 
+# Add a DNS Server to /etc/rsolv.conf to initialize internet connectivity
+# since it has not been initialized via a normal boot process.
+# Cloudflare users should consider using 1.1.1.1 instead, especially if
+# utilizing cloudflared/argo tunnel.
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 
-apt update
+# Update the apt package cache since it's not present on the ISO by default
+apt-get update
+
 # List of packages to be installed during build
-apt install -y \
+apt-get install -y \
         vim \
         htop
-        
-apt clean
 
+# Clean the apt-package cache to prevent stale package data on install
+# as weel as prevent inflating the image size
+apt-get clean
+
+# Remove our changes to the resolv.conf file
 echo ' ' > /etc/resolv.conf
 
+# Clear the history before exiting avoids leaking potentially sensitive data
 history -c
 exit
